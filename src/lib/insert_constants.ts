aaf294d48ed2c21d7a80032710634f678eae4e89
hopperelec <hopperelec@gmail.com>
2023-11-06 07:19:37 +0000

    Remove debugging remnant from insert_constants.ts

--- src/lib/insert_constants.ts
+++ src/lib/insert_constants.ts
@@ -2,7 +2,6 @@ import constants from "$lib/constants.ts";
 
 export default function (text: string): string {
   for (const [key, value] of Object.entries(constants)) {
-    console.log(key, value);
     text = text.replaceAll("%" + key + "%", value);
   }
   return text;
