a87512f4f1dd1a3e643a4d74f15d3452965cef39
hopperelec <hopperelec@gmail.com>
2024-06-02 22:59:37 +0100

    Implemented rolling text in hero

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,15 +1,85 @@
-<script>
+<script lang="ts">
 import HopperIcon from "$lib/components/hopper-logo/HopperIcon.svelte";
 import HopperelecText from "$lib/components/hopper-logo/HopperelecText.svelte";
+import { slide } from "svelte/transition";
+
+type STYLED_TEXT = { text: string; color: string };
+const rollingText: [
+	string, // verb
+	string?, // noun
+	string?, // connective
+	STYLED_TEXT?, // styled noun
+][] = [
+	["make", "websites", "with", { text: "HTML", color: "#f16524" }],
+	["make", "websites", "with", { text: "Svelte", color: "#ff3C00" }],
+	["make", "websites", "with", { text: "CSS", color: "#306af1" }],
+	["make", "websites", "with", { text: "SCSS", color: "#cf6c9c" }],
+	["make", "websites", "with", { text: "Javascript", color: "#f0db4f" }],
+	["make", "websites", "with", { text: "Typescript", color: "#377cc8" }],
+	["make", "websites", "with", { text: "PHP", color: "#7b7fb5" }],
+	["code", "stuff", "with", { text: "Python", color: "yellow" }],
+	["make", "a Discord bot", "with", { text: "Java", color: "#5382a1" }],
+	["develop", "Minecraft plugins", "with", { text: "Java", color: "#5382a1" }],
+	["contribute to", "open-source"],
+	["stream", "Bedwars", "on", { text: "YouTube", color: "red" }],
+	[
+		"administrate",
+		undefined,
+		undefined,
+		{ text: "Minecraft servers", color: "green" },
+	],
+	[
+		"administrate",
+		undefined,
+		undefined,
+		{ text: "Minecraft servers", color: "green" },
+	],
+	[
+		"administrate",
+		undefined,
+		undefined,
+		{ text: "Minecraft servers", color: "lime" },
+	],
+];
+function changeRollingText() {
+	let choice: number;
+	do choice = Math.floor(Math.random() * rollingText.length);
+	while (choice === i);
+	i = choice;
+	// Setting a variable causes Svelte to respond reactively even if it didn't change
+	// So I check if these changed manually before setting them
+	if (verb !== rollingText[i][0]) verb = rollingText[i][0];
+	if (noun !== rollingText[i][1]) noun = rollingText[i][1];
+	if (connective !== rollingText[i][2]) connective = rollingText[i][2];
+	if (styled !== rollingText[i][3]) styled = rollingText[i][3];
+}
+let i: number;
+let verb: string;
+let noun: string | undefined;
+let connective: string | undefined;
+let styled: STYLED_TEXT | undefined;
+changeRollingText();
+setInterval(changeRollingText, 5000);
 </script>
 
 <section id="hero">
   <HopperIcon fillColor="#646464" outlineColor="#fff" outlineWidth={6} typeOf3D="stroke" padding={{top: 3, right: 3, bottom: 3, left: 3}} scale={null}/>
-  <p>
-    Hi, I'm <span class="name-img"><HopperelecText/></span><br>
-    <!-- Verb and noun will be slots, these are just examples -->
-    I <span class="verb">code websites</span> in <span class="noun svelte">Svelte</span>
-  </p>
+  <p>Hi, I'm <span class="name-img"><HopperelecText/></span>. I</p>
+  <span id="rolling-text">
+    <!-- I want these to be inline, along with the "I", but I can't get that to work with transitions -->
+    {#key verb}
+      <div class="verb" transition:slide={{duration: 3000}}>{verb}</div>
+    {/key}
+    {#key noun}
+      <div class="noun" transition:slide={{duration: 3000}}>{noun || ''}</div>
+    {/key}
+    {#key connective}
+      <div class="connective" transition:slide={{duration: 3000}}>{connective || ''}</div>
+    {/key}
+    {#key styled}
+      <div style:color={styled?.color} transition:slide={{duration: 3000}}>{styled?.text || ''}</div>
+    {/key}
+  </span>
 </section>
 
 <svelte:head>
@@ -45,11 +115,11 @@ section {
   vertical-align: text-top;
 }
 
-.verb {
-  color: #aaa;
+.verb, .noun {
+  color: #fff;
 }
 
-.noun.svelte {
-  color: #FF3C00;
+.connective {
+  color: #aaa;
 }
 </style>
