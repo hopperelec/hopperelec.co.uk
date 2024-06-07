abb69fa01aaa042c14be8691d3cee11f468c65d8
hopperelec <hopperelec@gmail.com>
2024-06-07 21:10:34 +0100

    Moved all rolling text (in hero section) into just one transition:slide

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -3,12 +3,11 @@ import HopperIcon from "$lib/components/hopper-logo/HopperIcon.svelte";
 import HopperelecText from "$lib/components/hopper-logo/HopperelecText.svelte";
 import { slide } from "svelte/transition";
 
-type STYLED_TEXT = { text: string; color: string };
 const rollingText: [
 	string, // verb
 	string?, // noun
 	string?, // connective
-	STYLED_TEXT?, // styled noun
+	{ text: string; color: string }?, // styled noun
 ][] = [
 	["make", "websites", "with", { text: "HTML", color: "#f16524" }],
 	["make", "websites", "with", { text: "Svelte", color: "#ff3C00" }],
@@ -32,44 +31,31 @@ const rollingText: [
 	],
 ];
 function changeRollingText() {
-	let choice: number;
-	do choice = Math.floor(Math.random() * rollingText.length);
-	while (choice === i);
-	i = choice;
-	// Setting a variable causes Svelte to respond reactively even if it didn't change
-	// So I check if these changed manually before setting them
-	if (verb !== rollingText[i][0]) verb = rollingText[i][0];
-	if (noun !== rollingText[i][1]) noun = rollingText[i][1];
-	if (connective !== rollingText[i][2]) connective = rollingText[i][2];
-	if (styled !== rollingText[i][3]) styled = rollingText[i][3];
+	let choice = Math.floor(Math.random() * (rollingText.length - 1));
+	i = choice >= i ? choice + 1 : choice; // Don't pick same one
 }
 let i: number;
-let verb: string;
-let noun: string | undefined;
-let connective: string | undefined;
-let styled: STYLED_TEXT | undefined;
 changeRollingText();
-setInterval(changeRollingText, 5000);
+setInterval(changeRollingText, 6000);
 </script>
 
 <section id="hero">
   <HopperIcon fillColor="#646464" outlineColor="#fff" outlineWidth={6} typeOf3D="stroke" padding={{top: 3, right: 3, bottom: 3, left: 3}} scale={null}/>
   <p>Hi, I'm <span class="name-img"><HopperelecText/></span>. I</p>
-  <span id="rolling-text">
-    <!-- I want these to be inline, along with the "I", but I can't get that to work with transitions -->
-    {#key verb}
-      <div class="verb" transition:slide={{duration: 3000}}>{verb}</div>
-    {/key}
-    {#key noun}
-      <div class="noun" transition:slide={{duration: 3000}}>{noun || ''}</div>
-    {/key}
-    {#key connective}
-      <div class="connective" transition:slide={{duration: 3000}}>{connective || ''}</div>
-    {/key}
-    {#key styled}
-      <div style:color={styled?.color} transition:slide={{duration: 3000}}>{styled?.text || ''}</div>
-    {/key}
-  </span>
+  {#key i}
+    <p id="rolling-text" transition:slide={{duration: 2000}}>
+      <span class="verb">{rollingText[i][0]}</span>
+      {#if rollingText[i][1]}
+        <span class="noun">{rollingText[i][1]}</span>
+      {/if}
+      {#if rollingText[i][2]}
+        <span class="connective">{rollingText[i][2]}</span>
+      {/if}
+      {#if rollingText[i][3]}
+        <span style:color={rollingText[i][3]?.color}>{rollingText[i][3]?.text}</span>
+      {/if}
+    </p>
+  {/key}
 </section>
 
 <svelte:head>
@@ -105,11 +91,11 @@ section {
   vertical-align: text-top;
 }
 
-.verb, .noun {
+.noun {
   color: #fff;
 }
 
-.connective {
+.verb, .connective {
   color: #aaa;
 }
 </style>
